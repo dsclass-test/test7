c8443bca4506d33e8525dca4abad5e47a828eee2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 22:37:12 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,40 +1,42 @@
 
-df <- raw_data
-
-head(raw_data)
-
-#dfSVM <- df[,c(2:8,10,11,13,23:52)]
-dfSVM <- df[,c(1:3,4:7,10,12,13,15,25,26,28,29,31:39,40:54)]
-dfSVMTrain <- dfSVM[1:10000,1:40]
-nrow(dfSVMTrain)
-#c <- dfSVM[10001:12000,53]
-dfSVMTest <- dfSVM[10001:12000,1:40]
-nrow(dfSVMTest)
-#colnames(dfSVMTest)
-library(kernlab)
-
-#ksvm_Model <- ksvm(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning,data=dfSVMTrain, kernel = "rbfdot", kpar = "automatic", C = 10, cross = 1, prob.model = TRUE)
-
-ksvm_Model <- ksvm(StatisfactionType ~Scheduled.Departure.Hour_Early_Morning+Type.of.Travel_Mileage.tickets+Type.of.Travel_Business.travel+Flight.Distance+Flight.time.in.minutes+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Class+Shopping.Amount.at.Airport+No.of.Flights.p.a.+Year.of.First.Flight+Price.Sensitivity+Age+Gender+Airline.Status , data=dfSVMTrain, kernel = "rbfdot", kpar = "automatic", C = 10, cross = 10, prob.model = TRUE)
+library(dummies)
+df <- satisfaction_survey
 
 
-ksvm_Model
+df$Satisfaction[df$Satisfaction == "4.00.5" | df$Satisfaction == "4.00.2.00"] = 4
+df$Satisfaction <- as.double(df$Satisfaction)
 
-ksvmPredict <- predict(ksvm_Model, dfSVMTest)
-length(ksvmPredict)
-nrow(dfSVMTest)
+df$Departure.Delay.in.Minutes[df$Flight.cancelled=='Yes'] <- 1600
+df$Arrival.Delay.in.Minutes[df$Flight.cancelled=='Yes'] <- 1600
+df$Arrival.Delay.greater.5.Mins[df$Flight.cancelled=='Yes'] <- 'yes'
+df[is.na(df$Arrival.Delay.in.Minutes),]$Arrival.Delay.in.Minutes <- df[is.na(df$Arrival.Delay.in.Minutes),]$Departure.Delay.in.Minutes
+df[is.na(df$Flight.time.in.minutes),]$Flight.time.in.minutes <- 1000
+# View(df[df$Flight.cancelled== 'Yes',])
 
-dfSVMTest$predictedValues <- ksvmPredict
+df <- na.omit(df)
 
+df$Airline.Status <- as.numeric(factor(df$Airline.Status), levels = c('Blue', 'Silver', 'Gold','Platinum'))
+df$Gender <- ifelse(df$Gender=='Male',0,1)
+df$Year.of.First.Flight <- 2013-df$Year.of.First.Flight
+df$Type.of.Travel <- as.factor(df$Type.of.Travel)
+dfFlight <- dummies::dummy(df$Airline.Code,sep = "_")
+df <- cbind(df,dfFlight)
+dfTravel <- dummies::dummy(df$Type.of.Travel,sep = "_")
+df <- cbind(df,dfTravel)
+df$Class <- as.numeric(factor(df$Class), levels = c('Eco','Eco Plus','Business'))
+df$Scheduled.Departure.Hour <- cut(df$Scheduled.Departure.Hour, breaks = c(0,4,7,11.99,15.99,20,23),labels=c("Late_Night","Early_Morning","Morning","Afternoon","Evening","Night"))
+dfDepartureTime <- dummies::dummy(df$Scheduled.Departure.Hour,sep = "_")
+df <- cbind(df,dfDepartureTime)
+df$Arrival.Delay.greater.5.Mins <- ifelse(df$Arrival.Delay.greater.5.Mins=='no',0,1)
 
-#Function to check accuracy of predicted model
-functionForKSVMAccuracy <- function(dataSet){
-  a <- ifelse(dataSet$dfSVMTest.StatisfactionType == dataSet$dfSVMTest.predictedValues, 1,0)
-  return(length(which((a==1) == TRUE))/nrow(dataSet) * 100)
-}
+names(df) <- make.names(names(df), unique=TRUE)
 
-comparisonTable <- data.frame(dfSVMTest$StatisfactionType,dfSVMTest$predictedValues)
-table(comparisonTable)
-acc <- functionForKSVMAccuracy(comparisonTable)
-print(acc)
-acc
+df$StatisfactionType <- ""
+df[which(df$Satisfaction<3), which(colnames(df) == 'StatisfactionType')]<-"LOW"
+df[which(df$Satisfaction==3), which(colnames(df) == 'StatisfactionType')]<-"MED"
+df[which(df$Satisfaction>3), which(colnames(df) == 'StatisfactionType')]<-"HIGH"
+
+plot <- hist(df$Satisfaction)
+
+plot
+df
